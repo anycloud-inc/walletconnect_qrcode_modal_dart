8eb812f3345ad2498444d77a95ba4ba3f9662b8c
r9bt <r9bt0702@gmail.com>
2022-09-28 01:03:46 +0900

    URLコピーの変更

--- lib/src/components/modal_wallet_web_page.dart
+++ lib/src/components/modal_wallet_web_page.dart
@@ -4,28 +4,50 @@ import 'package:walletconnect_qrcode_modal_dart/src/lib/app_clipbord_manager.dar
 const _webAuthurl = 'https://app.cnpowners.jp';
 const _appGray = Color(0xFFA2A5A9);
 
-class ModalWalletWebPage extends StatelessWidget {
+class ModalWalletWebPage extends StatefulWidget {
   final Function() onPressed;
   const ModalWalletWebPage({
     required this.onPressed,
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<ModalWalletWebPage> createState() => _ModalWalletWebPageState();
+}
+
+class _ModalWalletWebPageState extends State<ModalWalletWebPage> {
+  bool _copiedToClipboard = false;
+
   Widget _urlText(BuildContext context) {
-    return GestureDetector(
-      onTap: () => AppClipboardManager.copy(context, text: _webAuthurl),
+    return Padding(
+      padding: const EdgeInsets.only(left: 16),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
+        children: [
+          const Text(
             _webAuthurl,
-            style: TextStyle(color: _appGray),
+            style: TextStyle(fontSize: 14, color: _appGray),
           ),
-          SizedBox(width: 8),
-          Icon(
-            Icons.content_copy,
-            size: 16,
-            color: _appGray,
+          const SizedBox(width: 8),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: _appGray.withOpacity(0.1),
+              minimumSize: Size.zero,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            child: Text(
+              _copiedToClipboard ? 'Copied' : 'Copy',
+              style: const TextStyle(fontSize: 12, color: _appGray),
+            ),
+            onPressed: _copiedToClipboard
+                ? null
+                : () async {
+                    await AppClipboardManager.copy(context, text: _webAuthurl);
+                    setState(() => _copiedToClipboard = true);
+                    await Future.delayed(
+                      const Duration(seconds: 1),
+                      () => setState(() => _copiedToClipboard = false),
+                    );
+                  },
           ),
         ],
       ),
@@ -37,7 +59,7 @@ class ModalWalletWebPage extends StatelessWidget {
       width: double.infinity,
       child: TextButton(
         child: const Text('QRコードを読み込む'),
-        onPressed: onPressed,
+        onPressed: widget.onPressed,
         style: ButtonStyle(
           padding: MaterialStateProperty.all(
               const EdgeInsets.symmetric(vertical: 16, horizontal: 16)),
